181ed3a7b9d20f27d6d0df94ab55708808d4fbf4
AdrianCes <adriances14@gmail.com>
2025-01-22 22:19:12 +0100

    Casi listo los nombres todos

--- src/Conexion/GeneradorBD.java
+++ src/Conexion/GeneradorBD.java
@@ -10,6 +10,41 @@ import java.util.Random;
 
 public class GeneradorBD {
 
+    String[] nombres = {
+        "Carlos", "María", "José", "Ana", "Luis","Elena", "Pedro", "Lucía", "Fernando", "Clara",
+        "Alberto", "Sara", "Raúl", "Laura", "Javier","Marta", "Ricardo", "Patricia", "Daniel", "Beatriz",
+        "Manuel", "Carmen", "Adrián", "Isabel", "Santiago","Victoria", "Diego", "Rosa", "Francisco", "Gloria",
+        "Jorge", "Sofía", "Rubén", "Paula", "Antonio","Julia", "Andrés", "Verónica", "Héctor", "Eva",
+        "Gabriel", "Silvia", "Rafael", "Alejandra", "Miguel","Teresa", "Iván", "Natalia", "Óscar", "Pilar"
+    };
+
+    String[] nombresPersonajes = {
+        "Mario", "Luigi", "Peach", "Bowser", "Link", "Zelda", "Ganondorf", "Samus", "Pikachu", "Ash", 
+        "Misty", "Brock", "Charizard", "Squirtle", "Bulbasaur", "Donkey Kong", "Diddy Kong", "Kirby", "Meta Knight", "King Dedede", 
+        "Fox McCloud", "Falco Lombardi", "Krystal", "Wolf O'Donnell", "Ness", "Lucas", "Captain Falcon", "Jigglypuff", "Marth", "Ike", 
+        "Chrom", "Robin", "Lucina", "Roy", "Corrin", "Byleth", "Shulk", "Pit", "Palutena", "Dark Pit", 
+        "Simon Belmont", "Richter Belmont", "Alucard", "Sonic", "Tails", "Knuckles", "Shadow", "Amy", "Dr. Eggman", "Crash Bandicoot", 
+        "Spyro", "Ryu", "Ken", "Chun-Li", "Guile", "Blanka", "E. Honda", "Dhalsim", "Zangief", "Cammy", 
+        "Vega", "M. Bison", "Jill Valentine", "Chris Redfield", "Leon Kennedy", "Ada Wong", "Claire Redfield", "Albert Wesker", "Dante", "Vergil", 
+        "Trish", "Bayonetta", "Jeanne", "Raiden", "Solid Snake","Big Boss", "Otacon", "Revolver Ocelot", "Liquid Snake", "The Boss", 
+        "Kratos", "Atreus", "Lara Croft", "Nathan Drake", "Ellie","Joel", "Tommy", "Abby", "Geralt de Rivia", "Yennefer", 
+        "Ciri", "Dandelion", "Triss Merigold", "V", "Johnny Silverhand","Panam Palmer", "Judy Alvarez", "Jackie Welles", "Goro Takemura", "Rogue"
+    };
+
+    String[] servidores = {
+        "ServidorCentral01","DataHub02","CloudNode03","BackupServer04",
+        "ProxyNode05","Database06","WebServer07",
+        "APIServer08","AuthServer09","FileServer10"
+    };
+
+    String[] mapas = {
+        "IslaTropical01","DesiertoArido02","CavernaOscura03","CiudadFuturista04",
+        "BosqueEncantado05","MontañaNevada06","PantanoSombrío07","RuinasAntiguas08",
+        "LagoCristalino09","VolcánActivo10","CañónRojo11","JunglaExótica12",
+        "CastilloMisterioso13","PlayaParadisíaca14","CamposDorados15","PáramoHelado16",
+        "FortalezaAbandonada17","CráterGigante18","CiudadSubterránea19","ZonaIndustrial20"
+    };
+
 
     private static final Random random = new Random();
     Connection conexion = Conexion.obtenerConexion();
@@ -17,7 +52,7 @@ public class GeneradorBD {
         try (PreparedStatement ps = conexion.prepareStatement("INSERT INTO Servidores (nombre, region) VALUES (? , ?)")){
             String[] regiones = {"Ámerica", "Europa", "África"};
             for (int i = 1 ; i <= 10; i++) {
-                ps.setString(1, "Servidor_" + i);
+                ps.setString(1, servidores[random.nextInt(servidores.length)]);
                 ps.setString(2, regiones[i % regiones.length]);
                 ps.addBatch();
             }
@@ -30,7 +65,7 @@ public class GeneradorBD {
     public void insertarUsuarios(){
         try (PreparedStatement ps = conexion.prepareStatement("INSERT INTO Usuarios (nombre, codigo_unico) VALUES (? , ?)")) {
             for (int i = 1; i <= 50; i++) {
-                ps.setString(1, "Usuario_" + i);
+                ps.setString(1, nombres[random.nextInt(nombres.length)]);
                 ps.setInt(2, generarCodigo());
                 ps.addBatch();
             }
@@ -43,7 +78,7 @@ public class GeneradorBD {
     public void insertarPersonajes(){
         try (PreparedStatement ps = conexion.prepareStatement("INSERT INTO Personajes (nombre, usuario_id, servidor_id) VALUES (? , ? , ?)")) {
             for (int i = 1; i <= 100; i++) {
-                ps.setString(1, "Personaje_" + i);
+                ps.setString(1, nombresPersonajes[random.nextInt(nombresPersonajes.length)]);
                 ps.setInt(2, random.nextInt(50)+1);
                 ps.setInt(3, random.nextInt(10)+1);
                 ps.addBatch();
@@ -57,7 +92,7 @@ public class GeneradorBD {
     public void insertarMapas(){
         try (PreparedStatement ps = conexion.prepareStatement("INSERT INTO Mapas (nombre, dificultad) VALUES (? , ?)")) {
             for (int i = 1; i <= 20; i++) {
-                ps.setString(1, "Nombre_" + i);
+                ps.setString(1, mapas[random.nextInt(mapas.length)]);
                 ps.setInt(2, random.nextInt(10));
                 ps.addBatch();
             }
--- src/TestConexion.java
+++ src/TestConexion.java
@@ -4,7 +4,9 @@ import Conexion.GeneradorBD;
 public class TestConexion {
     public static void main(String[] args) {
         CreadorBD.crearTablas();
-        GeneradorBD generar = new GeneradorBD();
-        generar.insertarDatos();
+        GeneradorBD gen = new GeneradorBD();
+        //gen.borrarTablas();
+        gen.insertarDatos();
+
     }
 }
